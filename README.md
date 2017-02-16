cc09af6b499682d131834f26b06d8678bc8ac6c3
Rene Hangstrup Møller <rhmoller@gmail.com>
2017-02-16 23:37:53 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Webpack loader for loading WebAssembly s-expressions from .wast files
+
+The loader returns a standard JavaScript array which should be converted to an Uint8Array before being passed to WebAssembly.compile().
---
+++ wast-loader.js
@@ -0,0 +1,10 @@
+var wast2wasm = require("wast2wasm");
+
+module.exports = function(wast) {
+    var callback = this.async();
+
+    wast2wasm(wast, true).then(function(out) {
+        var bytes = [].slice.call(out.buffer);
+        callback(null, "module.exports = " + JSON.stringify(bytes) + ";");
+    });
+};
